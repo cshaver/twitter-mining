1bf6c4e8825eda08379797acb8648983bdff2895
U-GLOBAL\cshaver <cshaver@H9470305329287.Global.ad.sabre.com>
2013-07-22 14:04:48 -0500

    final push

--- frontend/search.php
+++ frontend/search.php
@@ -1,390 +1,132 @@
 <?php
+function searchFlightNumbers($flightNum) {
+                
+                                mysql_connect("localhost", "root", "") or die(mysql_error()); 
+                                mysql_select_db("twitter_data") or die(mysql_error()); 
+                                
+                                $airlines = array();
+
+                                $result = mysql_query("SELECT * FROM airline_data WHERE flightNumber='$flightNum'");
+                                while($row = mysql_fetch_array( $result )) {
+                                                $id = $row["tweet_id"];
+
+                                                $airlines[$id]["airline"] = substr($row["flightNumber"], 0, 3); 
+                                                $airlines[$id]["flightNumber"] = $row["flightNumber"];
+                                                $airlines[$id]["date_from"] = $row["departure_time"];
+                                                $airlines[$id]["date_to"] = $row["arrival_time"];
+                                                $airlines[$id]["metric"] = $row["Metric"];
+                                }
+                                
+                                echo json_encode($airlines);
+                }
+                
+    function searchAirline($airline) {
+    
+                    mysql_connect("localhost", "root", "") or die(mysql_error()); 
+                    mysql_select_db("twitter_data") or die(mysql_error()); 
+                    
+                    $airline12 = array();
+                    $flightnum = substr($row["flightNumber"]);
+
+                    $result = mysql_query("SELECT * FROM airline_data WHERE field LIKE '%$flightnum%'");
+                    while($row = mysql_fetch_array( $result )) {
+                                    $id = $row["tweet_id"];
+
+                                    $airlines12[$id]["airline"] = $flightnum; 
+                                    $airlines12[$id]["flightNumber"] = $row["flightNumber"];
+                                    $airlines12[$id]["date_from"] = $row["departure_time"];
+                                    $airlines12[$id]["date_to"] = $row["arrival_time"];
+                                    $airlines12[$id]["metric"] = $row["Metric"];
+                    }
+                    
+                    echo json_encode($airlines12);
+    }
+                
+
 	header('Content-type: application/json');
-	$tweets = array(
+
+	mysql_connect("localhost", "root", "") or die(mysql_error()); 
+ 	mysql_select_db("twitter_data") or die(mysql_error()); 
+
+ 	//searchFlightNumbers($_POST['criteria']['flightNum']);
+ 	//searchFlightNumbers('qtr927');
+
+ 	$tweets = array();
+ 	$query ="SELECT * FROM airline_tweets";
+ 	// if (isset($_POST['criteria'])){
+	 // 	$flightNum = $_POST['criteria']['flightNum'];
+	 // 	$airline = $_POST['criteria']['airline'];
+
+ 	// }
+	// if (isset($_POST['criteria']['flightNum'])){
+	// 	$query = $query . "WHERE flightNumber='".$_POST['criteria']['flightNum']."'";
+	// } 
+ 	$result = mysql_query($query);
+
+ 	//$result = mysql_query("SELECT * FROM airline_tweets LIMIT 10000");
+    while($row = mysql_fetch_array( $result )) 
+	{ 
+		//print_r($row);
+		// build the array
+		$id = $row["ID"];
+		$tweets[$id] = array();
+
+		$tweets[$id]["text"] = $row["Tweet"];
+		$tweets[$id]["date"] = $row["Date"];
+		$tweets[$id]["username"] = $row["Username"];
+		$tweets[$id]["followers"] = $row["Num_Follower"];
+		$tweets[$id]["sentiment"] = $row["Sentiment_Type"];
+		$tweets[$id]["airline"] = "";
+		$tweets[$id]["flightNumber"] = "";
+		$tweets[$id]["origin"] = "";
+		$tweets[$id]["destination"] = "";
+		$tweets[$id]["keywords"] = $row["Keywords"];
+		$tweets[$id]["airline_data"] = array();
+
+		$max_metric = 0;
+ 		$airline_result = mysql_query("SELECT * FROM airline_data where tweet_id = ".$row["ID"]);
+    	while($airline_row = mysql_fetch_array( $airline_result )){
+			//print_r($airline_row);
+			$airline_id = $airline_row["id"];
+			$tweets[$id]["airline_data"][$airline_id]["airline"] = "";
+			$tweets[$id]["airline_data"][$airline_id]["flightNumber"] = $airline_row["flightNumber"];
+			$tweets[$id]["airline_data"][$airline_id]["origin"] = $airline_row["origin_airport"];
+			$tweets[$id]["airline_data"][$airline_id]["origin_city"] = $airline_row["origin_city"];
+			$tweets[$id]["airline_data"][$airline_id]["destination"] = $airline_row["dest_airport"];
+			$tweets[$id]["airline_data"][$airline_id]["destination_city"] = $airline_row["dest_city"];
+			$tweets[$id]["airline_data"][$airline_id]["metric"] = $airline_row["Metric"];
+
+			if ($airline_row["Metric"] > $max_metric)
+			{
+				$max_metric = $airline_row["Metric"];
+				$tweets[$id]["airline"] = "";
+				$tweets[$id]["flightNumber"] = $airline_row["flightNumber"];
+				$tweets[$id]["origin"] = $airline_row["origin_airport"];
+				$tweets[$id]["destination"] = $airline_row["dest_airport"];
+			}
+    	}
+
+	} 
+
+	//print_r($tweets);
+
+	/*$tweets = array(
 		"1234" => array (
 			"text" => "Hello i have a flight boarding",
 			"date" => "7/13/2013",
 			"sentiment" => "positive",
 			"airline" => "Spirit",
+			"origin" => "DFW",
+			"destination" => "SJC",
 			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"1111" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"2222" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"2345" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
-		"3456" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/13/2013",
-			"sentiment" => "positive",
-			"airline" => "Spirit",
-			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"4567" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"5678" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"5679" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
-		"6789" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/13/2013",
-			"sentiment" => "positive",
-			"airline" => "Spirit",
-			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"7890" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"227822" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"8907" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
-		"0987" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/13/2013",
-			"sentiment" => "positive",
-			"airline" => "Spirit",
-			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"9876" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"8765" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"7654" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
-		"6543" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/13/2013",
-			"sentiment" => "positive",
-			"airline" => "Spirit",
-			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"5432" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"4321" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"124334" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
-		"321" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/13/2013",
-			"sentiment" => "positive",
-			"airline" => "Spirit",
-			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"123482" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"383" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"1" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
-		"2" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/13/2013",
-			"sentiment" => "positive",
-			"airline" => "Spirit",
-			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"3" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"4" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"5" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
-		"6" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/13/2013",
-			"sentiment" => "positive",
-			"airline" => "Spirit",
-			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"7" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"8" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"9" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
-		"0" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/13/2013",
-			"sentiment" => "positive",
-			"airline" => "Spirit",
-			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"11" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"22" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"33" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
-		"44" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/13/2013",
-			"sentiment" => "positive",
-			"airline" => "Spirit",
-			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"55" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"66" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"667" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
-		"77" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/13/2013",
-			"sentiment" => "positive",
-			"airline" => "Spirit",
-			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"88" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"888" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"99" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
-		"000" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/13/2013",
-			"sentiment" => "positive",
-			"airline" => "Spirit",
-			"flightNumber" => "1234",
-			"keywords" => "hello, flight, boarding"
-			),
-		"123" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/14/2013",
-			"sentiment" => "neutral",
-			"airline" => "American",
-			"flightNumber" => "234",
-			"keywords" => "hello, something, boarding"
-			),
-		"718" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/15/2013",
-			"sentiment" => "positive",
-			"airline" => "Southwest",
-			"flightNumber" => "7532",
-			"keywords" => "what, plane, landing"
-			),
-		"381" => array (
-			"text" => "Hello i have a flight boarding",
-			"date" => "7/16/2013",
-			"sentiment" => "negative",
-			"airline" => "Southwest",
-			"flightNumber" => "0987",
-			"keywords" => "okay, cry, angry"
-			),
+			"keywords" => "hello, flight, boarding",
+			"airline_data" => array (
+					
+				)
+			)
 		);
+		*/
 	echo json_encode($tweets);
+
+
 ?>
